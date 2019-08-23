429ccfeaefddda8c9bdace89d05cd6762d97acb7
jonathanhand <hand.jonathan.13@gmail.com>
2019-08-22 21:26:03 -0700

    fixed reg expression to 4 digits b4

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const getPartsBtn = document.getElementById('getPartsBtn').addEventListener('click', function(){
     const emailField = document.getElementById('emailField');
-    parseParts(emailField.innerHTML);
+    console.log(emailField)
+    parseParts(emailField.value);
 
 });
 
 function parseParts(emailText) {
-    const pattern = /(\d{3,5})([a-z])(\d{1,3})/gim
+    const pattern = /(\d{4,5})([a-z])(\d{1,3})/gim
     const partMatch = emailText.match(pattern);
     createCSV(partMatch);
 
@@ -17,7 +18,7 @@ function createCSV (partMatch) {
     var partsCSV = ''
         for (let i in partMatch) {
         partsCSV = partsCSV + partMatch[i] + ',';
-
+            console.log(partsCSV)
     }
     const partCSVField = document.getElementById('partCSVField');
     partCSVField.innerHTML = partsCSV;
