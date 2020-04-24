7d679c01e43448ee2e23dd834c683b60afc80d6a
jonathanhand <hand.jonathan.13@gmail.com>
2020-04-24 12:55:56 -0700

    Integrated table maker

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+console.log("Testing thanks to:")
+console.log("Austen Young")
+console.log("Jack Gamboa")
+console.log("Patrick Potvin")
 function clearAll () {
 var elements = document.getElementsByTagName("input");
 var elements2 = document.getElementsByTagName('textarea');
@@ -36,10 +40,7 @@ const custField = document.getElementById('custField');
 const alert = document.getElementById('alert-text');
 //get html elements
 const getPartsBtn = document.getElementById('getPartsBtn').addEventListener('click', function () {
-    console.log("Testing thanks to:")
-    console.log("Austen Young")
-    console.log("Jack Gamboa")
-    console.log("Patrick Potvin")
+
     alert.style.display = "inline-block";
     const emailField = document.getElementById('emailField');
     const dupCheck = document.getElementById('dupCheck').checked;
@@ -492,4 +493,105 @@ function duplicateCheck(set, array) {
     }
     // console.log(duplicateAlerts)
 
+}
+
+var table = document.getElementById('tableOutput')
+
+const makeTableBtn = document.getElementById('makeTableBtn').addEventListener('click', function () {
+    const revField = document.getElementById('revField');
+    //const tableField = document.getElementById('tableField');
+    //const lineCheck = document.getElementById('lineCheck');
+    //const descriptionCheck = document.getElementById('descriptionCheck');
+    //const qtyCheck = document.getElementById('qtyCheck');
+    const availCheck = document.getElementById('availCheck').checked;
+
+
+    parseRev(revField.value, availCheck);
+});
+
+function parseRev (revVal, availCheck) {
+    let revSpace = revVal;
+    const tabPattern = /\t/;
+    const linePattern = /\n/;
+    let lineSplit = revSpace.split(linePattern);
+    let spaceList = revSpace.split(/\n/g);
+  let lineArray = []
+  let orderedArray = []
+  console.log('split!')
+  if(availCheck == false){
+  for (let line=0; line < lineSplit.length-2; line++) { 
+    let split = lineSplit[line].split(tabPattern)
+    lineArray[line] = split.slice();
+    console.log(split)
+ 
+    lineArray[line][0] = split.slice(0,1)
+    console.log(lineArray[line][0])
+    lineArray[line][1] = split.slice(3,4)
+    lineArray[line][2] = split.slice(4,5)
+    console.log(split[1])
+    if (split[6][0] != '$') {
+      split[6] = '$' + split[6]
+    }
+    lineArray[line][3] = split.slice(6,7)
+    lineArray[line][4] = split.slice(2,3)
+    lineArray[line][5] = split.slice(1,2)
+    lineArray[line][6] = split.slice(6,7)
+    lineArray[line][7] = split.slice(7,8)
+    lineArray[line][8] = split.slice(8,9)
+
+  }
+}
+else{
+  document.querySelectorAll('table td:nth-child(1)').style.display = "none";
+  for (let line=0; line < lineSplit.length-2; line++) { 
+    let split = lineSplit[line].split(tabPattern)
+    lineArray[line] = split.slice();
+    console.log(split)
+    lineArray[line][0] = split.slice(0,1)
+    console.log(lineArray[line][0])
+    lineArray[line][1] = split.slice(3,4)
+    lineArray[line][2] = split.slice(4,5)
+    console.log(split[1])
+    if (split[6][0] != '$') {
+      split[6] = '$' + split[6]
+    }
+    lineArray[line][3] = split.slice(6,7)
+    lineArray[line][4] = split.slice(2,3)
+    lineArray[line][5] = split.slice(1,2)
+    lineArray[line][6] = split.slice(6,7)
+    lineArray[line][7] = split.slice(7,8)
+    lineArray[line][8] = split.slice(8,9)
+    lineArray[line][9] = split[5]
+    console.log(lineArray[line][9])
+
+  }
+}
+  // for (let cell in lineArray) {
+  //   lineArray[]
+  // }
+  //console.log(lineArray)
+    
+    
+    
+    // console.log('TAB LIST: ')
+    // console.log(tabList)
+    // console.log(lineSplit)
+    createTable(lineArray)
+    
+}
+
+function createTable(lineArray) {
+  for (let row in lineArray) {
+    var tr = document.createElement('tr')
+    //console.log(lineArray[row])
+  for (let cell in lineArray[row]) {
+    var td= document.createElement('td')
+    //console.log(lineArray[row][cell])
+    td.appendChild(document.createTextNode(lineArray[row][cell]))
+    tr.appendChild(td)
+        }
+   table.appendChild(tr)
+
+    }
+  console.log(table)
 }
